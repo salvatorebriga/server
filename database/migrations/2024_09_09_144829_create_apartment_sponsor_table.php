4c1222688104d3ee3fc233482b4f30ac953e6742
salvatorebriga <salvo.z2302@gmail.com>
2024-09-09 16:56:30 +0200

    added pivot table apartment_sponsor

---
+++ database/migrations/2024_09_09_144829_create_apartment_sponsor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('apartment_id');
+            $table->unsignedBigInteger('sponsor_id');
+            $table->dateTimeTz('expir');
+            $table->timestamps('expiration date');
+
+            // Definire le chiavi esterne
+            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade');
+            $table->foreign('sponsor_id')->references('id')->on('sponsors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_sponsor');
+    }
+};

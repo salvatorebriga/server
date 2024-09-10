56229ab13c10c3ab77288d999f8b6a52e519a72f
salvatorebriga <salvo.z2302@gmail.com>
2024-09-10 17:05:55 +0200

    h2 modified

--- resources/views/auth/apartments/index.blade.php
+++ resources/views/auth/apartments/index.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      My Apartments
+    </h2>
+  </x-slot>
 
-    <h1>Apartments</h1>
+  <h1>Apartments</h1>
 
 </x-app-layout>

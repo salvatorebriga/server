428f5f6f9230e5aae00c8879a5fbef0996f08787
DanieleBernassola <danielebernassola02@gmail.com>
2024-09-10 16:54:52 +0200

    [UPD] Added apartments link

--- resources/views/auth/apartments/index.blade.php
+++ resources/views/auth/apartments/index.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-    </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        </h2>
+    </x-slot>
 
+    <h1>Apartments</h1>
 
 </x-app-layout>

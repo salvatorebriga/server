f5d3d31cdcd4ed789b9f45e6fdf8702ea4157b43
AlanDAmario <vega93@live.it>
2024-09-11 16:09:10 +0200

    create fix

--- resources/views/auth/apartments/create.blade.php
+++ resources/views/auth/apartments/create.blade.php
@@ -82,12 +82,19 @@
                 </div>
 
 
+                <div class="flex justify-between items-center mt-6">
+                    <!-- Back Button -->
+                    <a href="{{ route('apartments.index') }}"
+                        class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700">
+                        Back to List
+                    </a>
+
                 <!-- Submit Button -->
-                <div class="flex justify-end">
                     <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700">
                         Save Apartment
                     </button>
                 </div>
+
             </form>
         </div>
     </div>

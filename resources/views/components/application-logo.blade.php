5bd21ab9c9572cedbe0c069d308eeddd5cec770c
DanieleBernassola <danielebernassola02@gmail.com>
2024-09-12 16:40:34 +0200

    [UPD] Modified logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <img src="{{ asset('img/Airbnb_Logo_Bélo.svg.png') }}" alt="Logo" class="max-w-20"
+    <img src="{{ asset('img/boolbnb-logo.png') }}" alt="Logo" class="max-w-20"
         style="filter: brightness(100%) saturate(100%)" />
 </div>

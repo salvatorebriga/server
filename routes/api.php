62763766741f61655022375c70d357cb655e32dd
salvatorebriga <salvo.z2302@gmail.com>
2024-09-13 14:42:08 +0200

    removed front end auth routes

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,6 @@
 
 use App\Http\Controllers\Api\ApartmentsController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Models\Apartment;
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
 
 Route::get('/apartments', [ApartmentsController::class, 'index'])->name('apartments.index');
 Route::get('/apartments/{id}', [ApartmentsController::class, 'show'])->name('apartments.show');

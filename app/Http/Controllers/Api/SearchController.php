a61b90a0fef4d07c102fb7b3f3e10c699dcdf26e
AlanDAmario <vega93@live.it>
2024-09-13 14:49:39 +0200

    add fix on searchcontroller

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;

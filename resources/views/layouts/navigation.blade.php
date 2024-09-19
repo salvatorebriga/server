ede373492196534399a50730f53ac0f2c8982159
AlanDAmario <vega93@live.it>
2024-09-19 12:31:31 +0200

    add logo on dashboard, and icon logo for style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <!-- Inserisci qui il percorso del tuo logo -->
+                        <img src="{{ asset('img/boolbnb-logo.png') }}" alt="Logo" class=" h-20 w-auto" />
                     </a>
                 </div>
 

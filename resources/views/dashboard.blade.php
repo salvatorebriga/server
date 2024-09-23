e5ba84b7840f96dbb81015edb5aecc35d0c5ba1e
AlanDAmario <vega93@live.it>
2024-09-23 16:27:10 +0200

    add style, to index apartment, and confirm modal on delete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header"> 
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>

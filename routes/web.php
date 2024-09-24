1511ac5b33a302da1e3fd83a12f05cebe6608daa
AlanDAmario <vega93@live.it>
2024-09-24 22:27:24 +0200

    add original welcome, fix route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\Auth\DashboardController as ControllersDashboardControl
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', [ControllersDashboardController::class, 'index'])

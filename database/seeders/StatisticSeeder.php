d3b9d115620b262212c5b893867731c54c836e63
salvatorebriga <salvo.z2302@gmail.com>
2024-09-24 21:44:10 +0200

    added stats seeder

--- database/seeders/StatisticSeeder.php
+++ database/seeders/StatisticSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Apartment;
+use App\Models\Statistic;
+use Carbon\Carbon;
 
 class StatisticSeeder extends Seeder
 {
@@ -12,6 +14,42 @@ class StatisticSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $apartments = Apartment::all();
+
+        if ($apartments->isEmpty()) {
+            $this->command->info('Nessun appartamento trovato nel database.');
+            return;
+        }
+
+        $startDate = Carbon::now()->subDays(364);
+
+        for ($day = 0; $day < 365; $day++) {
+            $currentDate = $startDate->copy()->addDays($day);
+
+            foreach ($apartments as $apartment) {
+                $views = rand(1, 20);
+
+                for ($i = 0; $i < $views; $i++) {
+                    $ipAddress = $this->generateRandomIp();
+
+                    Statistic::create([
+                        'apartment_id' => $apartment->id,
+                        'ip_address' => $ipAddress,
+                        'created_at' => $currentDate,
+                        'updated_at' => $currentDate,
+                    ]);
+                }
+            }
+        }
+
+        $this->command->info('Statistiche di visualizzazione generate con successo.');
+    }
+
+    /**
+     * @return string
+     */
+    private function generateRandomIp()
+    {
+        return implode('.', array_map('intval', array_rand(range(0, 255), 4)));
     }
 }
